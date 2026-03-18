1cb77254d985cecf1a0b7e0cdc2580506f8e82df
Bruno M <kgosipakomogome@gmail.com>
2026-03-18 15:19:59 +0200

    Initial2

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person{
+    private String name;
+    private String id;
+
+    public Person(String name, String id){
+        this.name = name;
+        this.id = id;
+    } 
+    public String getName(){
+        return this.name;
+    }
+    public String getId(){
+        return this.id;
+    }
+    public String toString(){
+        return this.name + " (" + this.id + ")";
+    }
+}  
