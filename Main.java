e65f9e50f7686d18ff97fdcb6f8d600f73d8b7ea
Bruno M <kgosipakomogome@gmail.com>
2026-03-18 20:29:11 +0200

    adding lecturer to main

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
-        Student S = new Student("Alice", "12345", 34.5);
-        System.out.println();
+        Scanner pk = new Scanner(System.in);
 
-}}
+        System.out.println("Are you a student or lecturer? (s/l)");
+        String role = pk.nextLine();
+
+        if (role.equalsIgnoreCase("S")) {
+            Student S = new Student("Alice", "12345", 34.5);
+            System.out.println(S.getInfo());
+        }
+        else if(role.equalsIgnoreCase("L")){
+            Lecturer l = new Lecturer("Pako", "37766", "Comp");
+            System.out.println(l.getInfo());
+        }
+    }
+}
