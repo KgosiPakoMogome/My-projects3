bd5d385d04fa4c3220621d77c09b2fb3ee84fdf2
Bruno M <kgosipakomogome@gmail.com>
2026-03-18 19:17:40 +0200

    adding main class

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        Person person1 = new Person("Alice", "12345");
-        System.out.println(person1);
+        Student S = new Student("Alice", "12345", 34.5);
+        System.out.println();
 
 }}
