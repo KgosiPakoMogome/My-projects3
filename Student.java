22dec2f85513aab3e60c30da41cf2af86e652497
Bruno M <kgosipakomogome@gmail.com>
2026-03-18 19:13:00 +0200

    adding Student class

---
+++ Student.java
@@ -0,0 +1,22 @@
+public class Student extends Person{
+    
+    private double gpa;
+
+    public Student(String name, String id, double gpa){
+        super(name, id);
+        setGpa(gpa);
+
+    }
+    public void setGpa(double gpa){
+        if (gpa > 5.0 || gpa < 0.0){
+            this.gpa = 0.0;
+        }
+        else {
+            this.gpa = gpa;
+        }
+    }
+    @Override
+    public String getInfo(){
+        return getInfo() + "Gpa :" + gpa;
+    }
+}
